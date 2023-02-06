dfe4b35a30548b2c11104fa18f1772c12d17c963
kandalog <asuasu.tee@gmail.com>
2023-02-07 00:03:15 +0900

    新規投稿機能実装

--- main.go
+++ main.go
@@ -22,11 +22,28 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/todos", getTodos)
+	router.POST("/todos", postTodo)
 
 	// ":8080"
 	router.Run()
 }
 
+// 全てのTodoを取得
 func getTodos(c *gin.Context) {
+
 	c.IndentedJSON(http.StatusOK, todos)
+}
+
+// 新規投稿
+func postTodo(c *gin.Context) {
+	// bodyの内容が入る
+	var newTodo todo
+
+	// JSONを構造体にbindする
+	if err := c.BindJSON(&newTodo); err != nil {
+		return
+	}
+
+	todos = append(todos, newTodo)
+	c.IndentedJSON(http.StatusCreated, newTodo)
 }
